1807e18be610b950a0241343bbd96886f79db78a
swathisurapally <151178254+swathisurapally@users.noreply.github.com>
2023-11-17 23:45:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,67 @@
-document.getElementById('registrationForm').addEventListener('submit', function(event) {
-    
+document.getElementById("registrationForm").addEventListener("submit", function(event) {
     event.preventDefault();
-
     
-    var name = document.getElementById('name').value;
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
-    var dob = document.getElementById('dob').value;
-    var terms = document.getElementById('terms').checked;
-
-	document.getElementById("fg").innerHTML+=`<tr><td style="border:1px solid black ; border-collapse:collapse">${name}</td><td style="border:1px solid black ; border-collapse:collapse">${email}</td><td style="border:1px solid black ; border-collapse:collapse">${password}</td><td style="border:1px solid black ; border-collapse:collapse">${dob}</td><td style="border:1px solid black ; border-collapse:collapse">${terms ? 'Yes' : 'No'}</td></tr>`
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var dob = document.getElementById("dob").value;
+    var terms = document.getElementById("terms").checked;
     
-    document.getElementById('name').value = '';
-    document.getElementById('email').value = '';
-    document.getElementById('password').value = '';
-    document.getElementById('dob').value = '';
-    document.getElementById('terms').checked = false;
-});
+    if (!validateEmail(email)) {
+      alert("Invalid email address!");
+      return;
+    }
+    
+    var age = calculateAge(dob);
+    if (age < 18 || age > 55) {
+      alert("Age must be between 18 and 55!");
+      return;
+    }
+    
+    if (!terms) {
+      alert("Please accept the terms and conditions!");
+      return;
+    }
+    
+    addUserToTable(name, email, password, dob, terms);
+    saveDataToLocalStorage(name, email, password, dob, terms);
+    clearForm();
+  });
+  
+  function validateEmail(email) {
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
+  
+  function calculateAge(dob) {
+    var today = new Date();
+    var birthDate = new Date(dob);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var monthDiff = today.getMonth() - birthDate.getMonth();
+    
+    if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+    }
+    
+    return age;
+  }
+  
+  function addUserToTable(name, email, password, dob, terms) {
+    var table = document.getElementById("userTable");
+    var row = table.insertRow(-1);
+    
+    var nameCell = row.insertCell(0);
+    nameCell.innerHTML = name;
+    
+    var emailCell = row.insertCell(1);
+    emailCell.innerHTML = email;
+    
+    var passwordCell = row.insertCell(2);
+    passwordCell.innerHTML = password;
+    
+    var dobCell = row.insertCell(3);
+    dobCell.innerHTML = dob;
+    
+    var termsCell = row.insertCell(4);
+    termsCell.innerHTML = terms ? "Yes" : "No";
+  }
